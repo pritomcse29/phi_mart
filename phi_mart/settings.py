33e38053fd6eda92a6f34d3d4c63ca5b9da9d408
Pritom Sarkar <pritom.cse.29@gmail.com>
2025-04-28 01:25:59 +0600

    Cors added

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -48,11 +48,13 @@ INSTALLED_APPS = [
     'order',
     'api',
     'django_filters',
+     "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -63,6 +65,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'phi_mart.urls'
 
+CORS_ALLOWED_ORIGINS = [
+
+    'http://localhost:5173',
+]
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
