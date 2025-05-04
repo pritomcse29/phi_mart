aacc2134747e4b2090c777468afd4411455726e4
Pritom Sarkar <pritom.cse.29@gmail.com>
2025-05-04 15:27:50 +0600

    settings updated

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -200,7 +200,7 @@ DJOSER = {
     'EMAIL_FRONTEND_SITE_NAME':'PhiMart',
     'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
     'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
-    'ACTIVATION_URL': '/activate/{uid}/{token}',
+    'ACTIVATION_URL': 'activate/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL': True,
     'SERIALIZERS':{
         'user_create':'users.serializers.UserCreateSerializer',
